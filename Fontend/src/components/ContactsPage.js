ec70005ebe8c02ea32c3fba08c8a5c4e25f213d3
nour az <naz.199860@gmail.com>
2022-06-27 03:04:57 +0300

    contacts display

--- Fontend/src/components/ContactsPage.js
+++ Fontend/src/components/ContactsPage.js
@@ -1,8 +1,53 @@
-import React from 'react'
-
+import { async } from "q";
+import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const ContactsPage = () => {
+  const navigate =useNavigate();
+  const[contacts,setContacts]=useState([]);
+  const[details,setDetails]=useState([]);
+
+
+  const getContacts=()=>{
+    const res= await fetch("http://localhost:3001/api/contacts//getconts:id");
+    const data=await res.json();
+    return data;
+  };
+
+
+  useEffect(()=>{
+    const getData=async()=>{
+      const conts=await getContacts();
+      setContacts(conts);
+
+    };
+    getData;
+  },[])
   return (
-    <div>ContactsPage</div>
+    <div className="contactsContainer">
+      Contacts
+      {contacts.map((contact)=>{
+        return(
+          <div key={contact._id}
+          onClick={()=>{
+            navigate(`${contact._id}`);
+          }}>
+            <div className="CC">
+            <h3>{contact.f_name } {contact.l_name}</h3>
+            <div className="contDetails">
+              <h3>Phone: {contact.phone}</h3>
+              <h3>Relationship Status: {contact.RelationshipStatus}</h3>
+              <h3>Email: {contact.email}</h3>
+              <div>Location: 
+
+              </div>
+
+            </div>
+          </div>
+          </div>
+        );
+      })}
+
+    </div>
   )
 }
 
